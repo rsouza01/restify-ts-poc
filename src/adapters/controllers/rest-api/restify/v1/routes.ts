45da27b959ae88ea7d32e2881fb8ef06fa1cfbcc
Rodrigo de Souza <rsouza01@gmail.com>
2021-01-13 22:39:01 +0000

    Intermediary work

--- src/adapters/controllers/rest-api/restify/v1/routes.ts
+++ src/adapters/controllers/rest-api/restify/v1/routes.ts
@@ -3,17 +3,12 @@ import { Server, Request, Response, Next, RouteOptions, RequestHandlerType, Rout
 
 import { handlers } from './use-case-controllers'
 
-
 function handler(req: Request, res: Response, next: Next): void {
-
 	// res.send(await produtoService.list());
-
 	// const controller = new SignupController({});
 	// return controller.handler(event, context);
-	res.send({});
-
 	console.debug(`>>>>>>>> CALLED <<<<<<<<<<<`);
-
+	res.send({});
 }
 
 const registerRoutes = (server: Server) => {
@@ -25,13 +20,15 @@ const registerRoutes = (server: Server) => {
 		'get': server.get
 	};
 
-	console.debug(`HANDLERS => ${JSON.stringify(handlers, null, 2)}`);
-	console.debug(`RESULT => ${JSON.stringify(handlers[0].handler({} as Request, {} as Response), null, 2)}`);
+	const register = server.post;
+	console.debug(`REGISTERS => ${JSON.stringify(registers, null, 2)}`);
 
+	console.debug(`HANDLERS => ${JSON.stringify(handlers, null, 2)}`);
 
 	handlers.forEach(apiHandler => {
+		register(apiHandler.endpoint, apiHandler.handler);
 		// registers[apiHandler.verb](apiHandler.endpoint, apiHandler.handler);
-		server.post(apiHandler.endpoint, apiHandler.handler);
+		// server.post(apiHandler.endpoint, apiHandler.handler);
 
 		// console.debug(`registers[apiHandler.verb] => ${JSON.stringify(registers[apiHandler.verb], null, 2)}`);
 
